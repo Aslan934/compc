908729073bc50195ea0bc0c754c0ea213cb478c3
unknown <aslan.rustemov777@gmail.com>
2022-04-07 21:05:02 +0400

    Changed debug state

--- compc/settings.py
+++ compc/settings.py
@@ -26,7 +26,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'FALSE'
+DEBUG = False
 
 ALLOWED_HOSTS = ['compc.pythonanywhere.com', '127.0.0.1']
 
@@ -131,6 +131,7 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
