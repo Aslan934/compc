766a3cb3378c4f05715147425c8f657b802d18a9
unknown <aslan.rustemov777@gmail.com>
2022-04-08 03:32:49 +0400

    updated settings

--- compc/settings.py
+++ compc/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # ALLOWED_HOSTS = ['compc.pythonanywhere.com', '127.0.0.1']
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
